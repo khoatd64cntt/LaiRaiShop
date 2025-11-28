3796fa3fcb67cb2af5f837461739281be6e95038
ThongNguyen-64132336 <thong.nd.64cntt@ntu.edu.vn>
2025-11-28 23:45:57 +0700

    Merge pull request #14 from khoatd64cntt/thong/update
    Thong/update

---
+++ js/ad_banner1.js
@@ -0,0 +1,10 @@
+// Hàm hiện thông báo
+function kichHoatUuDai() {
+  const modal = document.getElementById("thongBaoModal");
+  modal.classList.add("show");
+}
+
+// Hàm chuyển trang
+function veTrangChu() {
+  window.location.href = "../homepage.php";
+}
