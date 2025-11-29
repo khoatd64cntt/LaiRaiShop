f585f62f42a9573063717431f661abc3750a2741
Doan Thanh Tu <tu.dt.64cntt@ntu.edu.vn>
2025-11-29 15:59:42 +0700

    fix: bug

--- page/SellerPage/dashboard.php
+++ page/SellerPage/dashboard.php
@@ -85,7 +85,8 @@ $recent_orders = $conn->query($sql_recent);
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard - Kênh Người Bán</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kênh Người Bán - Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <style>
         /* CSS Cũ */
